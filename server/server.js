ea25c8448f3a32f3cb8c501a7b5d942ccc4c9faa
Greyson Frazier <greyson.frazier@gmail.com>
2019-01-19 00:46:23 -0500

    add POST users/login route

--- server/server.js
+++ server/server.js
@@ -103,8 +103,6 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 
-
-
 app.post('/users', (req,res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -119,17 +117,24 @@ app.post('/users', (req,res) => {
 
 });
 
-// app.get('/users', (req,res) => {
-//   User.find().then((users) => {
-//     res.send({users});
-//   }, (e) =>{
-//     res.status(400).send(e);
-//   });
-// });
-
 app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
 });
+
+//POST /users/login {email, password}
+app.post('/users/login', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email,body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth',token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
